702a4094d659ed3767cd55d2111766a9e66ad46b
mateo.l05 <Mateolalin2020@gmail.com>
2025-03-27 14:40:42 -0300

    correccion de variables

---
+++ ejercicio.php
@@ -0,0 +1,9 @@
+<?<php>
+$nombreCOmpleto = "juean perez" // todo okey
+$edad = 25; // todo okey
+$estAtura = 1.75; // nombraria la variable $ estatura para que no halla ningun disparejo visual
+$numero = "10"; // las comillas en los decimales no son utilizadas 
+Apellido = `fernndez`; // las comillas franzesas solo se utilizan en java script no en visual, y no esta marcada la variable con el simbolo de pesos "$"
+pais_de_origuen= "uruguay" // no se declaro la variable, y no se utilizaan las barras bajas en las variables
+$salario = $2000 // no se le pone ningun adorno al nuero solo se utiizan decimales
+?>
