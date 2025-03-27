19ae5221157b3eba65459b44a9ee5ca0fad9ebc8
mateo.l05 <Mateolalin2020@gmail.com>
2025-03-27 15:17:04 -0300

    definir varibles

--- ejercicio.php
+++ ejercicio.php
@@ -6,4 +6,7 @@ $numero = "10"; // las comillas en los decimales no son utilizadas
 Apellido = `fernndez`; // las comillas franzesas solo se utilizan en java script no en visual, y no esta marcada la variable con el simbolo de pesos "$"
 pais_de_origuen= "uruguay" // no se declaro la variable, y no se utilizaan las barras bajas en las variables
 $salario = $2000 // no se le pone ningun adorno al nuero solo se utiizan decimales
+
+define('gravedad', 9.8); const gravedad = 9.8; // es mas facil y sencillo declarar una variable Y definir su valor antes que declararle una un valor a una variabl que puede cambiar 
+
 ?>
