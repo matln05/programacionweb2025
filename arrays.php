c67f6c5f05c22665771bd669fb8bf68634652456
mateo.l05 <Mateolalin2020@gmail.com>
2025-05-22 15:50:57 -0300

    .

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<?php
+$arrayVacio=[]
+$arrayVacio[2]='algo'
+var_dump($arrayVacio);
+
+$planetas[];
+$planetas[] = 'Marte';
+ver_dump($planetas);
+$planetas[] = 'Tierra';
+ver_dump($planetas);
+$planetas[] = 'Venus'
+ver_dump($planetas);
+
+foreach ($planetas as $planetas){
+    echo $planetas . "<br>";
+}
+?>
