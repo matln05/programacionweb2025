6d75fd35889ae73d3c3103c335bc1034d1cd4442
mateo.l05 <Mateolalin2020@gmail.com>
2025-07-29 17:39:13 -0300

    .

---
+++ ArreglosAsociativos.php
@@ -0,0 +1,11 @@
+<?php
+$Videojuegos =[
+    $Juego = "The Legend Of Zelda"
+    $Consola = "NES"
+    $Año = "2023"
+    $Precio = "50.00"
+]
+foreach($Videojuegos as $key => $Value){
+}
+echo $Value
+?>
