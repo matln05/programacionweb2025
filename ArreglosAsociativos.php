5668eaed2653df88facde30dcb2bcafd4f4cde0f
mateo.l05 <Mateolalin2020@gmail.com>
2025-07-29 17:57:39 -0300

    .

--- ArreglosAsociativos.php
+++ ArreglosAsociativos.php
@@ -1,11 +1,17 @@
 <?php
 $Videojuegos =[
     $Juego = "The Legend Of Zelda"
-    $Consola = "NES"
-    $Año = "2023"
+    $Consola => "NES"
+    $Año = "1986"
     $Precio = "50.00"
 ]
 foreach($Videojuegos as $key => $Value){
-}
 echo $Value
+}
+
+$Videojuegos["stok"] = 40
+$Videojuegos["venta"] ='Verdadero';
+foreach($Videojuegos as $key => $Value){
+echo $Value
+}
 ?>
