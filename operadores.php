88cf0cd0fd03b9a7c3ac4c3e5170163138d0fc38
mateo.l05 <Mateolalin2020@gmail.com>
2025-09-17 20:30:49 -0300

    .

--- operadores.php
+++ operadores.php
@@ -4,15 +4,15 @@ $edad += 1;
 echo $edad;// suma la variable que halla en edad y le suma uno y lo guerda nuevamente en edad.
 
 $edad = 1;
-$edad -= 10
+$edad -= 10;
 echo $edad;//resta la variable que halla en edad y le resta 10 y lo guerda nuevamente en edad.
 
 $edad = 1;
-$edad *= 10
+$edad *= 10;
 echo $edad;//multiplica la variable que halla en edad y lo multiplica por 10 y lo guerda nuevamente en edad.
 
 $edad = 1;
-$edad /= 10
+$edad /= 10;
 echo $edad;//divide la variable que halla en edad y lo divide por 10 y lo guerda nuevamente en edad.
 
 $saludo .= "_hola";
